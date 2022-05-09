2c5d6ad4bb8f91bf512b834ee448aba9c376ba48
SumiAkter2 <akteralisha36@gmail.com>
2022-05-09 19:57:43 +0600

    heroku deploy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors=require('cors');
 require('dotenv').config();
@@ -10,8 +11,24 @@ app.use(express.json());
 
 
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hacav.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('mongodb connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
 app.get('/',(req,res)=>{
-    res.send('Running this code')
+    res.send('Running this  code easily')
 })
 
 app.listen(port,()=>{
