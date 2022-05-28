59fc7489a1a276db5fdb9d0d34cb4ed792210bad
SumiAkter2 <akteralisha36@gmail.com>
2022-05-28 20:44:05 +0600

    added order collection

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
-const cors=require('cors');
+const cors = require('cors');
 require('dotenv').config();
-const port=process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hacav.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -24,6 +19,7 @@ async function run() {
     try {
         await client.connect();
         const collection = client.db("carLand").collection("products");
+        const orderCollection = client.db("carLand").collection("orders");
 
         app.get('/products', async (req, res) => {
             const query = {};
@@ -31,25 +27,25 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         });
-
-        app.get('/products/:id', async(req, res) =>{
+        //comment
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await collection.findOne(query);
             res.send(service);
         });
 
         // POST:::::::::
-        app.post('/products', async(req, res) =>{
+        app.post('/orders', async (req, res) => {
             const newService = req.body;
             const result = await collection.insertOne(newService);
             res.send(result);
         });
 
-       // DELETE:::::::::::
-        app.delete('/products/:id', async(req, res) =>{
+        // DELETE:::::::::::
+        app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await collection.deleteOne(query);
             res.send(result);
         });
@@ -65,10 +61,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Running this  code easily')
+app.get('/', (req, res) => {
+    res.send('Running this  code very easily')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(port);
 })
